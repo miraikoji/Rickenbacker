466ecc7098a87bf652001ee0a3d42caae5fb7e2f
Taro Aoyama <t.aoyama32@gmail.com>
2023-07-11 14:24:00 +0900

    first comit

---
+++ frontend/svelte-app/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import {
+    Theme,
+    RadioButtonGroup,
+    RadioButton,
+  } from "carbon-components-svelte";
+
+	import type {
+    CarbonTheme
+  } from "carbon-components-svelte/types/Theme/Theme.svelte";
+
+  let theme: CarbonTheme = "g90";
+</script>
+
+<Theme bind:theme />
+
+<RadioButtonGroup legendText="Carbon theme" bind:selected={theme}>
+  {#each ["white", "g10", "g80", "g90", "g100"] as value}
+    <RadioButton labelText={value} {value} />
+  {/each}
+</RadioButtonGroup>
+
+<h1>Welcome to Rickenbacker !!</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
