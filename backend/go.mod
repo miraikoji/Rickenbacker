9bd49cd1a4da972909ae1a960a659c7d71d14459
t4traw <t4traw@gmail.com>
2023-10-18 16:02:14 +0900

    ログインセッションまで

--- backend/go.mod
+++ backend/go.mod
@@ -5,9 +5,16 @@ go 1.20
 require github.com/labstack/echo v3.3.10+incompatible
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/labstack/echo-contrib v0.15.0 // indirect
+	github.com/labstack/echo/v4 v4.10.2 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -17,6 +24,7 @@ require (
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	gorm.io/driver/mysql v1.5.1 // indirect
 	gorm.io/gorm v1.25.2 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -3,13 +3,96 @@ package main
 import (
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type User struct {
+	UserId   string
+	Password string
+}
+
+type LoginRequest struct {
+	UserId   string `json:"userId"`
+	Password string `json:"password"`
+}
+
+// 動作確認用のデータ
+// 実際のアプリケーションでは、データベースを使う
+var users = map[string]string{
+	"t4traw": "$2a$10$5VotD2mOBoRj2At0wG7bw.qSZgylGZydJoEP38fqQyiRphsqf8NLa",
+}
 
 func main() {
 	e := echo.New()
+	e.Logger.SetLevel(log.DEBUG)
+
+	store := sessions.NewCookieStore([]byte("secret-key"))
+	store.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   86400,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	}
+
+	e.Use(session.Middleware(store))
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Rickenbacker!!")
 	})
+
+	e.GET("/login", loginPageHandler)
+	e.POST("/login", loginHandler)
+	e.GET("/secure_page", securePageHandler, checkLoginMiddleware)
+
 	e.Logger.Fatal(e.Start(":9090"))
+}
+
+func loginPageHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Please login")
+}
+
+func loginHandler(c echo.Context) error {
+	req := LoginRequest{}
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid request")
+	}
+
+	userId := req.UserId
+	password := req.Password
+
+	hashedPassword, exists := users[userId]
+	if !exists {
+		return c.String(http.StatusUnauthorized, "Invalid userId or Password")
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		return c.String(http.StatusUnauthorized, "Invalid userId or Password")
+	}
+
+	sess, _ := session.Get("session", c)
+	sess.Values["logged_in"] = true
+	sess.Values["user_id"] = userId
+	sess.Save(c.Request(), c.Response())
+
+	return c.String(http.StatusOK, "Logged in!")
+}
+
+func securePageHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "secure page")
+}
+
+func checkLoginMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		sess, _ := session.Get("session", c)
+		if loggedIn, ok := sess.Values["logged_in"].(bool); !ok || !loggedIn {
+			return c.Redirect(http.StatusSeeOther, "/login")
+		}
+		return next(c)
+	}
 }
