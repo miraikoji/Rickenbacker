93ba4caa568657ea51260837e073fa06934e3232
t4traw <t4traw@gmail.com>
2023-10-24 18:59:35 +0900

    ひとまずtailwindで表示するところまで

---
+++ frontend/svelte-app/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+	import Header from '../components/Header.svelte';
+</script>
+
+<Header />
+
+<div class="mx-auto max-w-7xl p-6 lg:px-8">
+	<slot />
+</div>
--- frontend/svelte-app/src/routes/+page.svelte
+++ frontend/svelte-app/src/routes/+page.svelte
@@ -1,24 +1,7 @@
-<script lang="ts">
-  import {
-    Theme,
-    RadioButtonGroup,
-    RadioButton,
-  } from "carbon-components-svelte";
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
 
-	import type {
-    CarbonTheme
-  } from "carbon-components-svelte/types/Theme/Theme.svelte";
-
-  let theme: CarbonTheme = "g90";
-</script>
-
-<Theme bind:theme />
-
-<RadioButtonGroup legendText="Carbon theme" bind:selected={theme}>
-  {#each ["white", "g10", "g80", "g90", "g100"] as value}
-    <RadioButton labelText={value} {value} />
-  {/each}
-</RadioButtonGroup>
-
-<h1>Welcome to Rickenbacker !!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
