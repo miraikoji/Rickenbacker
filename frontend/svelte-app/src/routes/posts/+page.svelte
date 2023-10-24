f5498a4bc8fe79f5c6217dfa8484a14357a7db52
t4traw <t4traw@gmail.com>
2023-10-24 11:43:08 +0900

    Postの一覧をひとまず表示

--- frontend/svelte-app/src/routes/posts/+page.svelte
+++ frontend/svelte-app/src/routes/posts/+page.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	let posts: any[] = [];
+	type Post = {
+		ID: number;
+		CreatedAt: string;
+		UpdatedAt: string;
+		DeletedAt: string | null;
+		Title: string;
+		Body: string;
+		UserID: number;
+	};
+
+	let posts: Post[] = [];
 
 	const fetchPosts = async () => {
-		const response = await fetch('http://backend:9090/posts');
+		const response = await fetch('http://localhost:9090/posts');
+
 		if (response.ok) {
 			posts = await response.json();
 		} else {
 			console.error('Failed to fetch posts');
 		}
-
-		onMount(() => {
-			fetchPosts();
-		});
 	};
+
+	onMount(() => {
+		fetchPosts();
+	});
 </script>
 
 <h1>Posts</h1>
