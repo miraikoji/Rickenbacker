bafcfb41522018cf693f087d12ca859ef3d1b1b9
Seiji Kaneko <hitati5774@gmail.com>
2023-11-21 02:48:41 +0900

    tmp

--- backend/controllers/posts.go
+++ backend/controllers/posts.go
@@ -39,11 +39,7 @@ func (con *PostsController) CreatePost(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, nil)
 	}
 
-	sh := SessionController{DB: con.DB}
-	user, err := sh.CurrentUser(c)
-	if err != nil {
-		return c.JSON(http.StatusNotFound, "User Not Found")
-	}
+	user := c.Get("CurrentUser").(*models.User)
 	post.UserID = user.ID
 
 	if result := con.DB.Create(&post); result.Error != nil {
@@ -59,11 +55,7 @@ func (con *PostsController) UpdatePost(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, nil)
 	}
 
-	sh := SessionController{DB: con.DB}
-	user, err := sh.CurrentUser(c)
-	if err != nil {
-		return c.JSON(http.StatusNotFound, "User Not Found")
-	}
+	user := c.Get("CurrentUser").(*models.User)
 	post.UserID = user.ID
 
 	if result := con.DB.Save(&post); result.Error != nil {
